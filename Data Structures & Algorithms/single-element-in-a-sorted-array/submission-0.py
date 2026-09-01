01af42cfe9596eab30f5687e010d29070f8373f1
sangeetha lunavath <23b81a6744@cvr.ac.in>
2026-09-01 13:22:41 +0530

    Add: single-element-in-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums.count(nums[i])==1:
+                return nums[i]
+        
