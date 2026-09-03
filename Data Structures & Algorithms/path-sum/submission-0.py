cfe99b050bb1b567fdaceb68c226646807efbc53
sangeetha lunavath <23b81a6744@cvr.ac.in>
2026-09-03 18:25:49 +0530

    Add: path-sum - submission-0

---
+++ Data Structures & Algorithms/path-sum/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+        if root is None:
+            return False
+
+        if root.left is None and root.right is None:
+            return root.val == targetSum
+
+        return (self.hasPathSum(root.left, targetSum - root.val) or
+                self.hasPathSum(root.right, targetSum - root.val))
